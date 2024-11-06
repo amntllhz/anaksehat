9df448a75f27a65ebce24d87460ad88e4682f05c
amntllhz <amanatullah1904@gmail.com>
2024-11-06 18:21:08 +0700

    layout: navbar base

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,13 +8,35 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './node_modules/flowbite/**/*.js'
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                in : "'Inter var', sans-serif",
+            },
+            colors : {        
+                prim : '#35C6A8',
+                sec : '#FFA17A',       
             },
         },
+        screens: {
+            sm: '640px', // -> @media (min-width: 640px) { ... }
+            md: '768px', // -> @media (min-width: 768px) { ... }
+            lg: '1024px', // -> @media (min-width: 1024px) { ... }
+            xl: '1280px', // -> @media (min-width: 1280px) { ... }
+            '2xl': '1536px', // -> @media (min-width: 1536px) { ... }
+        },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin'),
+
+        function({ addUtilities }) {
+            addUtilities({
+              '.font-feature-settings-cv11': {
+                'font-feature-settings': '"cv11"',
+              },
+            }, ['responsive', 'hover']);
+          },
+    ],
 };
