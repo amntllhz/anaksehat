4c8d0282a23ea05fff10dbf53bc11325ec321c79
amntllhz <amanatullah1904@gmail.com>
2024-11-16 17:48:06 +0700

    adm: img del

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Article extends Model
 {    
     // Menambahkan kolom yang boleh diisi massal
     protected $fillable = ['judul', 'deskripsi', 'gambar','konten'];
+
+    // Event menghapus gambar saat artikel dihapus
+    protected static function booted()
+    {
+        static::deleting(function ($article) {
+            // Menghapus file gambar dari storage/public jika ada
+            if ($article->gambar) {
+                Storage::disk('public')->delete($article->gambar);
+            }
+        });
+    }
 }
