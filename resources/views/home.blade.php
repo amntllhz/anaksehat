7f7806ee0e2e1319b05f2b5b0c0d2a9b1fd08aab
amntllhz <amanatullah1904@gmail.com>
2024-11-28 09:44:25 +0700

    layout: feedback modal fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,15 +23,12 @@
   <!-- Modal toggle -->
   @if (session('success'))
     <div id="default-modal" tabindex="-1" aria-hidden="true" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-        <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative p-7 w-full max-w-md max-h-full">
             <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <div class="relative bg-white rounded-xl shadow sm:max-w-sm dark:bg-gray-700">
 
                 <!-- Modal header -->
-                <div class="flex items-center justify-between p-4 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-lg font-semibold text-center text-gray-900 dark:text-white">
-                        Berhasil
-                    </h3>
+                <div class="flex items-center justify-end p-4 border-b rounded-t dark:border-gray-600">                    
                     <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
                         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
